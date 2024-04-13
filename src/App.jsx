6c27eb44dbffa7ea99ee5d9d1e883b78c79a05e3
marcosAlvarezCalabria <marcosalvarezcalabria@gmail.com>
2024-04-13 14:01:10 +0100

    Feat : iteration 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,27 @@
+import { Route,Routes } from "react-router-dom";
 import "./App.css";
+import CountryDetails from "./pages/CountryDetailsPage";
+import HomePage from "./pages/HomePage";
+import Navbar from "./components/Navbar";
+
+
+
 
 function App() {
   return (
     <div className="App">
       <h1>LAB | React WikiCountries</h1>
+      <Navbar/>
+     
+      
+      <Routes>
+        <Route path="/" element={<HomePage/>}></Route>
+        <Route path="/:countryId" element={<CountryDetails/>}></Route>
+      </Routes>
+
     </div>
   );
 }
+
 
 export default App;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,3 +1,24 @@
-function HomePage() {}
+
+
+function HomePage() {
+
+    return(
+        <>
+        <h2>WikiCountries: Your Guide to the World</h2>
+        <div className="container" style={{maxHeight: "90vh",overflow: "scroll"}}>
+        <h1 style={{fontSize: "24px"}}>WikiCountries: Your Guide to the World</h1>
+
+        <div className="list-group">
+          <a className="list-group-item list-group-item-action" href="/ABW"
+            >🇦🇼 Aruba</a>
+         
+        </div>
+      </div>
+        </>
+    
+    
+    )
+    
+}
 
 export default HomePage;
